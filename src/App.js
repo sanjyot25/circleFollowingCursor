0f68ef9428a495ac58d1f4a6dd668372ff8252f1
sanjyot25 <sanjyotvelip25@gmail.com>
2024-10-27 18:15:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Cursor from "./components/cursor";
+import {useState} from "react"
+import "./styles.css";
+
+export default function App() {
+
+const [ scaling, setScaling] =  useState()
+
+  return (
+    <div className="App">
+     <Cursor scaling={scaling}/>
+     <section>
+      <button onMouseEnter={()=> setScaling(true)} onMouseLeave={()=>{setScaling(false)}}>read more</button>
+     </section>
+    </div>
+  );
+}
