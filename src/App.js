2ddb7b746e8288c43c0d41160ed745a80269c8a7
sanjyot25 <sanjyotvelip25@gmail.com>
2024-10-27 18:18:37 +0530

    formatting code struc

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Cursor from "./components/cursor";
-import {useState} from "react"
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-
-const [ scaling, setScaling] =  useState()
+  const [scaling, setScaling] = useState();
 
   return (
     <div className="App">
-     <Cursor scaling={scaling}/>
-     <section>
-      <button onMouseEnter={()=> setScaling(true)} onMouseLeave={()=>{setScaling(false)}}>read more</button>
-     </section>
+      <Cursor scaling={scaling} />
+      <section>
+        <button
+          onMouseEnter={() => setScaling(true)}
+          onMouseLeave={() => {
+            setScaling(false);
+          }}
+        >
+          read more
+        </button>
+      </section>
     </div>
   );
 }
